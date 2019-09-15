b53a8989c59ba269e70764ffbb9935a8caacf7d7
FJ98 <felix.solano@utec.edu.pe>
2019-09-15 03:07:43 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/mutant_flatworld_explorers_118_FJ98.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mutant_flatworld_explorers_118_FJ98.dir/main.cpp.o"
+  "mutant_flatworld_explorers_118_FJ98.pdb"
+  "mutant_flatworld_explorers_118_FJ98"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mutant_flatworld_explorers_118_FJ98.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
